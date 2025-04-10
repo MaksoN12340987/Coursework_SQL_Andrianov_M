12e7b9df18a5cd29b0c1348bb8b2b1cb46f57d01
Maksim Andreev <andrianov_maksim@outlook.com>
2025-04-10 10:08:54 +0300

    add metods in class DBManager + docstring + get_vacancies_with_higher_salary

--- src/db_manager.py
+++ src/db_manager.py
@@ -1,41 +1,80 @@
 import logging
 
+import requests as re
+
 from src.base_classes import Manager
 
-logger_main = logging.getLogger(__name__)
+logger_db_manager = logging.getLogger(__name__)
 file_handler = logging.FileHandler(f"log/{__name__}.log", mode="w", encoding="UTF8")
 file_formatter = logging.Formatter(
     "\n%(asctime)s %(levelname)s %(name)s \n%(funcName)s %(lineno)d: \n%(message)s", datefmt="%H:%M:%S %d-%m-%Y"
 )
 file_handler.setFormatter(file_formatter)
-logger_main.addHandler(file_handler)
-logger_main.setLevel(logging.INFO)
+logger_db_manager.addHandler(file_handler)
+logger_db_manager.setLevel(logging.INFO)
 
 
 class DBManager(Manager):
+    
+    url: str
 
-    def __init__(self):
-        '''получает список всех компаний и количество вакансий у каждой компании'''
-        pass
+    def __init__(self, url):
+        ''''''
+        self.__url = url
+        self.__params = {"text": "", "page": 1, "per_page": 1}
+        self.__headers = {"User-Agent": "HH-User-Agent"}
+        self.__vacancies = []
 
     def __str__(self):
-        '''получает список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию'''
+        ''''''
         pass
 
     def __call__(self, *args, **kwds):
+        ''''''
+        pass
+
+    def get_companies_and_vacancies_count(self):
+        '''получает список всех вакансий с указанием названия компании, названия вакансии
+        и зарплаты и ссылки на вакансию'''
+        pass
+
+    def get_all_vacancies(self):
         '''получает среднюю зарплату по вакансиям'''
         pass
 
-    def get_companies_and_vacancies_count():
+    def get_avg_salary(self):
         '''получает список всех вакансий, у которых зарплата выше средней по всем вакансиям'''
         pass
 
-    def get_all_vacancies():
-        pass
+    def get_vacancies_with_higher_salary(self, search_word: str = ""):
+        '''получает список всех вакансий, в названии которых содержатся переданные в метод слова'''
+        self.__params['text'] = search_word
+        while self.__params.get("page") != 20:
+            vacancies = []
+            try:
+                response = re.get(self.__url, headers=self.__headers, params=self.__params)
+                if response.status_code == 200:
+                    vacancies = response.json()["items"]
 
-    def get_avg_salary():
-        pass
+            except re.exceptions.ConnectionError:
+                vacancies = []
+                raise ConnectionError("Connection Error. Please check your network connection.")
 
-    def get_vacancies_with_higher_salary():
-        '''получает список всех вакансий, в названии которых содержатся переданные в метод слова, например python'''
-        pass
+            except re.exceptions.HTTPError:
+                vacancies = []
+                raise ValueError("HTTP Error. Please check the URL.")
+
+            finally:
+                logger_db_manager.info(f"Attempt to access API completed, returned:\n{vacancies[:50]}")
+
+            self.__vacancies.extend(vacancies)
+            self.__params["page"] += 1
+            
+            for i, value in enumerate(self.__pull_vacanci):
+                try:
+                    result += f"{i + 1}. {value["name"]} {value["salary"]["from"]}\n"
+                except TypeError:
+                    result += f"{i + 1}. {value["name"]} {value["salary"]}\n"
+            
+        return self.__vacancies
+
